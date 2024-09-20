a7dbe4438739bfcc60ff7530b6d75f23f7aed9a6
sofijares <sofijar@aia.ac>
2024-09-20 09:26:31 +0200

    Revert "Added page factory"
    This reverts commit cb8c9d88bc3ad5f07b095eeefd18e6cd59d61218.

--- cypress/e2e/checkboxes.cy.js
+++ cypress/e2e/checkboxes.cy.js
@@ -1,14 +1,10 @@
 /// <reference types="Cypress" >/
-//import LoginPage from '../pages/loginpage'
-//import checkboxes from '../pages/checkboxes'
-import PageFactory from '../pagefactory'
+import LoginPage from '../pages/loginpage'
+import checkboxes from '../pages/checkboxes'
 
 describe('Checkbox verification using POM', ()=> {
     
     let LoginInfo
-    const LoginPage = PageFactory.createPage('login')
-    const checkboxes = PageFactory.createPage('checkbox')
-
     before(() => {
     cy.getAllSessionStorage()
     cy.fixture('param').then((data) =>{
@@ -20,9 +16,11 @@ describe('Checkbox verification using POM', ()=> {
     it('Login into herokuapp', function() {
 
         cy.visit('https://the-internet.herokuapp.com/login')
+        cy.wait(1000)
         LoginPage.EnterUsername(LoginInfo.username)
         LoginPage.EnterPassword(LoginInfo.password)
         LoginPage.loginBtn()
+        cy.wait(1000)
 
       })
     
@@ -30,9 +28,13 @@ describe('Checkbox verification using POM', ()=> {
 
         //Verify that checkbox 2 is automatically selected after logging into account
         cy.visit('https://the-internet.herokuapp.com/checkboxes')
+        cy.wait(1000)
         cy.get('[checked=""]').should('be.checked')
+        cy.wait(1000)
         checkboxes.ClickOnCheckbox.ClickCheckbox1()
+        cy.wait(1000)
         checkboxes.ClickOnCheckbox.ClickCheckbox2()
+        cy.wait(5000)
       })
 
       after(() => {
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,8 @@
 /// <reference types="Cypress" >/
-//import LoginPage from '../pages/loginpage'
-import PageFactory from '../pagefactory'
+import LoginPage from '../pages/loginpage'
 
 describe('Parameterized login test using POM', ()=> {
   let LoginInfo
-  const LoginPage = PageFactory.createPage('login')
   before(() => {
   cy.getAllSessionStorage()
   cy.fixture('param').then((data) =>{
@@ -17,18 +15,22 @@ describe('Parameterized login test using POM', ()=> {
   it('Login into herokuapp', function() {
 
     cy.visit('https://the-internet.herokuapp.com/login')
+    cy.wait(1000)
     LoginPage.EnterUsername(LoginInfo.username)
     LoginPage.EnterPassword(LoginInfo.password)
     LoginPage.loginBtn()
+    cy.wait(1000)
     LoginPage.loginBtn()
     ///Test to get invalid username error message
     LoginPage.elements.UsernameInput().should('be.visible').type('Timothy Smith')
     LoginPage.EnterPassword(LoginInfo.password)
     LoginPage.loginBtn()
+    cy.wait(2000)
     ///Test to get invalid password error message
     LoginPage.EnterUsername(LoginInfo.username)
     LoginPage.elements.PasswordInput().should('be.visible').type('This should work, right?')
     LoginPage.loginBtn()
+    cy.wait(5000)
   })
   
   after(() => {
